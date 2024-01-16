a6e024d157ca14ae8d9abad279cc938295e3bd4d
Ankush Patel <patelankush.2012@gmail.com>
2024-01-16 17:45:22 -0600

    init prefers color scheme state

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { MdHome, MdOutlineHome, MdOutlineLightMode, MdDarkMode } from 'react-icons/md';
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,12 +1,21 @@
-"use client"
+"use client";
 
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    // Check if window is defined (client-side)
+    if (typeof window !== 'undefined') {
+      // Set initial mode based on prefers-color-scheme
+      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      return prefersDarkMode;
+    }
+    // Default to false if window is not defined (e.g., during server-side rendering)
+    return false;
+  });
+  
   const toggleTheme = () => {
     setIsDarkMode((prevDarkMode) => !prevDarkMode);
   };
