ba8a4c581341748d0f712f89640dc3384378c6bb
Ankush Patel <patelankush.2012@gmail.com>
2024-01-16 19:14:52 -0600

    typescript shenanigans

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,26 +2,31 @@
 
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
-const ThemeContext = createContext();
+interface ThemeContextProps {
+  isDarkMode: boolean;
+  toggleTheme: () => void;
+}
 
-export const ThemeProvider = ({ children }) => {
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider: React.FC<RootLayoutProps> = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
-    // Check if window is defined (client-side)
     if (typeof window !== 'undefined') {
-      // Set initial mode based on prefers-color-scheme
       const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
       return prefersDarkMode;
     }
-    // Default to false if window is not defined (e.g., during server-side rendering)
     return false;
   });
-  
+
   const toggleTheme = () => {
     setIsDarkMode((prevDarkMode) => !prevDarkMode);
   };
 
   useEffect(() => {
-    // Update data-theme attribute based on the theme
     document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
   }, [isDarkMode]);
 
@@ -32,7 +37,7 @@ export const ThemeProvider = ({ children }) => {
   );
 };
 
-export const useTheme = () => {
+export const useTheme = (): ThemeContextProps => {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error('useTheme must be used within a ThemeProvider');
