091408df315cfd6f17e1825ec1e6e9e4b3f13783
Ankush Patel <patelankush.2012@gmail.com>
2024-01-30 17:39:51 -0600

    update footer

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -36,15 +36,13 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
 
   return (
     <a
-      className="text-sm text-gray-500 transition hover:text-gray-600"
+      className="text-sm text-secondary transition hover:text-secondary/70"
       target="_blank"
       rel="noopener noreferrer"
       href={href}
     >
       <span className="sr-only">{kind}</span>
-      <SocialSvg
-        className={`fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400 h-${size} w-${size}`}
-      />
+      <SocialSvg className={`fill-secondary hover:fill-accent h-${size} w-${size}`} />
     </a>
   )
 }
