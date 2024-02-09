db20f19e781abee544391211734cd64ed55a5a07
Ankush Patel <patelankush.2012@gmail.com>
2024-02-08 18:42:11 -0600

    add robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,23 @@
+import { MetadataRoute } from "next";
+import { allBlogs } from "contentlayer/generated";
+import siteMetadata from "@/data/siteMetadata";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = siteMetadata.siteUrl;
+
+  const blogRoutes = allBlogs
+    .filter((post) => !post.draft)
+    .map((post) => ({
+      url: `${siteUrl}/blog/posts/${post.slug}`,
+      lastModified: post.lastmod || post.date,
+    }));
+
+  const routes = ["", "about", "blog", "blog/posts", "blog/tags"].map(
+    (route) => ({
+      url: `${siteUrl}/${route}`,
+      lastModified: new Date().toISOString().split("T")[0],
+    }),
+  );
+
+  return [...routes, ...blogRoutes];
+}
