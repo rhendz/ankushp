13bfeb02af425397925f77e05c8c2c97e364c379
Ankush Patel <patelankush.2012@gmail.com>
2024-03-04 20:19:50 -0600

    add quantization diagrams

--- components/blog/diagram.tsx
+++ components/blog/diagram.tsx
@@ -2,15 +2,38 @@ import React, { Suspense } from 'react'
 
 interface DiagramProps {
   src: string
+  caption: string
+  style?: React.CSSProperties
 }
 
-const Diagram: React.FC<DiagramProps> = ({ src }) => {
+const Diagram: React.FC<DiagramProps> = ({ src, caption, style }) => {
   const LazyDiagram = React.lazy(() => import(`${src}`))
+
+  const extractNumber = (str) => {
+    const match = str.match(/\d+/)
+    return match ? parseInt(match[0], 10) + 1 : null
+  }
+
+  const figureNumber = extractNumber(src)
 
   return (
     <>
       <Suspense fallback={<div className="text-secondary">Loading...</div>}>
-        <LazyDiagram />
+        {figureNumber !== null && (
+          <div
+            id={`figure-${figureNumber}`}
+            className="flex-1 flex-col items-center justify-center py-8"
+          >
+            <div style={style}>
+              <LazyDiagram />
+            </div>
+
+            <p className="text-center">
+              <span className="font-bold">Figure {figureNumber}: </span>
+              <span className="italic">{caption}</span>
+            </p>
+          </div>
+        )}
       </Suspense>
     </>
   )
--- components/blog/foundations-of-quantization/diagram01.tsx
+++ components/blog/foundations-of-quantization/diagram01.tsx
@@ -1,32 +1,15 @@
 'use client'
 import * as React from 'react'
-const Diagram00 = (props) => (
+const Diagram01 = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlSpace="preserve"
-    viewBox="0 0 100.01 36.802"
+    viewBox="0 0 100.16 36.791"
     {...props}
   >
     <defs>
       <marker
-        id="h"
-        markerHeight={1}
-        markerWidth={1}
-        orient="auto"
-        overflow="visible"
-        preserveAspectRatio="xMidYMid"
-        viewBox="0 0 1 1"
-      >
-        <path
-          fill="context-fill"
-          fillRule="evenodd"
-          stroke="context-stroke"
-          strokeWidth={0.9}
-          d="M2.25 0a2.25 2.25 0 0 1-4.5 0c0-1.242 1.035-2.25 2.25-2.25A2.25 2.25 0 0 1 2.25 0z"
-        />
-      </marker>
-      <marker
-        id="g"
+        id="b"
         markerHeight={1}
         markerWidth={1}
         orient="auto-start-reverse"
@@ -35,41 +18,11 @@ const Diagram00 = (props) => (
         viewBox="0 0 1 1"
       >
         <path
-          fill="context-fill"
+          fill="context-stroke"
           fillRule="evenodd"
           stroke="context-stroke"
-          strokeWidth={1}
+          strokeWidth=".5pt"
           d="M2.885 0-1.44 2.5v-5z"
-        />
-      </marker>
-      <marker
-        id="c"
-        markerHeight={1}
-        markerWidth={1}
-        orient="auto"
-        overflow="visible"
-        preserveAspectRatio="xMidYMid"
-        viewBox="0 0 1 1"
-      >
-        <path
-          fill="context-stroke"
-          fillRule="evenodd"
-          d="M2.5 0a2.5 2.5 0 0 1-5 0c0-1.38 1.15-2.5 2.5-2.5A2.5 2.5 0 0 1 2.5 0z"
-        />
-      </marker>
-      <marker
-        id="d"
-        markerHeight={1}
-        markerWidth={1}
-        orient="auto"
-        overflow="visible"
-        preserveAspectRatio="xMidYMid"
-        viewBox="0 0 1 1"
-      >
-        <path
-          fill="context-stroke"
-          fillRule="evenodd"
-          d="M2.5 0a2.5 2.5 0 0 1-5 0c0-1.38 1.15-2.5 2.5-2.5A2.5 2.5 0 0 1 2.5 0z"
         />
       </marker>
       <marker
@@ -90,10 +43,10 @@ const Diagram00 = (props) => (
         />
       </marker>
       <marker
-        id="b"
+        id="d"
         markerHeight={1}
         markerWidth={1}
-        orient="auto-start-reverse"
+        orient="auto"
         overflow="visible"
         preserveAspectRatio="xMidYMid"
         viewBox="0 0 1 1"
@@ -101,9 +54,22 @@ const Diagram00 = (props) => (
         <path
           fill="context-stroke"
           fillRule="evenodd"
-          stroke="context-stroke"
-          strokeWidth=".5pt"
-          d="M2.885 0-1.44 2.5v-5z"
+          d="M2.5 0a2.5 2.5 0 0 1-5 0c0-1.38 1.15-2.5 2.5-2.5A2.5 2.5 0 0 1 2.5 0z"
+        />
+      </marker>
+      <marker
+        id="c"
+        markerHeight={1}
+        markerWidth={1}
+        orient="auto"
+        overflow="visible"
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 0 1 1"
+      >
+        <path
+          fill="context-stroke"
+          fillRule="evenodd"
+          d="M2.5 0a2.5 2.5 0 0 1-5 0c0-1.38 1.15-2.5 2.5-2.5A2.5 2.5 0 0 1 2.5 0z"
         />
       </marker>
       <marker
@@ -137,7 +103,7 @@ const Diagram00 = (props) => (
         />
       </marker>
       <marker
-        id="p"
+        id="h"
         markerHeight={1}
         markerWidth={1}
         orient="auto"
@@ -154,41 +120,7 @@ const Diagram00 = (props) => (
         />
       </marker>
       <marker
-        id="o"
-        markerHeight={1}
-        markerWidth={1}
-        orient="auto-start-reverse"
-        overflow="visible"
-        preserveAspectRatio="xMidYMid"
-        viewBox="0 0 1 1"
-      >
-        <path
-          fill="context-fill"
-          fillRule="evenodd"
-          stroke="context-stroke"
-          strokeWidth={1}
-          d="M2.885 0-1.44 2.5v-5z"
-        />
-      </marker>
-      <marker
-        id="r"
-        markerHeight={1}
-        markerWidth={1}
-        orient="auto"
-        overflow="visible"
-        preserveAspectRatio="xMidYMid"
-        viewBox="0 0 1 1"
-      >
-        <path
-          fill="context-fill"
-          fillRule="evenodd"
-          stroke="context-stroke"
-          strokeWidth={0.9}
-          d="M2.25 0a2.25 2.25 0 0 1-4.5 0c0-1.242 1.035-2.25 2.25-2.25A2.25 2.25 0 0 1 2.25 0z"
-        />
-      </marker>
-      <marker
-        id="q"
+        id="g"
         markerHeight={1}
         markerWidth={1}
         orient="auto-start-reverse"
@@ -306,6 +238,74 @@ const Diagram00 = (props) => (
           d="M2.885 0-1.44 2.5v-5z"
         />
       </marker>
+      <marker
+        id="p"
+        markerHeight={1}
+        markerWidth={1}
+        orient="auto"
+        overflow="visible"
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 0 1 1"
+      >
+        <path
+          fill="context-fill"
+          fillRule="evenodd"
+          stroke="context-stroke"
+          strokeWidth={0.9}
+          d="M2.25 0a2.25 2.25 0 0 1-4.5 0c0-1.242 1.035-2.25 2.25-2.25A2.25 2.25 0 0 1 2.25 0z"
+        />
+      </marker>
+      <marker
+        id="o"
+        markerHeight={1}
+        markerWidth={1}
+        orient="auto-start-reverse"
+        overflow="visible"
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 0 1 1"
+      >
+        <path
+          fill="context-fill"
+          fillRule="evenodd"
+          stroke="context-stroke"
+          strokeWidth={1}
+          d="M2.885 0-1.44 2.5v-5z"
+        />
+      </marker>
+      <marker
+        id="r"
+        markerHeight={1}
+        markerWidth={1}
+        orient="auto"
+        overflow="visible"
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 0 1 1"
+      >
+        <path
+          fill="context-fill"
+          fillRule="evenodd"
+          stroke="context-stroke"
+          strokeWidth={0.9}
+          d="M2.25 0a2.25 2.25 0 0 1-4.5 0c0-1.242 1.035-2.25 2.25-2.25A2.25 2.25 0 0 1 2.25 0z"
+        />
+      </marker>
+      <marker
+        id="q"
+        markerHeight={1}
+        markerWidth={1}
+        orient="auto-start-reverse"
+        overflow="visible"
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 0 1 1"
+      >
+        <path
+          fill="context-fill"
+          fillRule="evenodd"
+          stroke="context-stroke"
+          strokeWidth={1}
+          d="M2.885 0-1.44 2.5v-5z"
+        />
+      </marker>
     </defs>
     <g strokeWidth={0.202}>
       <g fill="none">
@@ -314,22 +314,22 @@ const Diagram00 = (props) => (
           strokeDasharray="0.808195, 0.808195"
           markerEnd="url(#a)"
           markerStart="url(#b)"
-          d="M72.868 100.5h69.566"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="M154.65 100.5h69.566"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
         <path
           stroke="#4ac683"
           markerEnd="url(#c)"
           markerStart="url(#d)"
-          d="M72.922 122.66h70.114"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="M154.57 122.66h70.114"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
         <path
           stroke="#4ac683"
           markerEnd="url(#e)"
           markerStart="url(#f)"
-          d="M88.247 100.5h38.808"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="M170.03 100.5h38.808"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
       </g>
       <path
@@ -337,45 +337,48 @@ const Diagram00 = (props) => (
         stroke="rgb(var(--color-secondary)/0.8)"
         markerEnd="url(#g)"
         markerStart="url(#h)"
-        d="m107.65 100.51-.063 21.25"
-        transform="translate(-92.127 -127.577) scale(1.3095)"
+        d="M189.43 100.51v21.274"
+        transform="translate(-199.077 -127.585) scale(1.3095)"
+      />
+      <path
+        fill="#ff7f2a"
+        stroke="rgb(var(--color-secondary)/0.8)"
+        strokeOpacity={0.941}
+        markerEnd="url(#g)"
+        markerStart="url(#h)"
+        d="m178.74 100.51-5.918 21.305"
+        transform="translate(-199.077 -127.585) scale(1.3095)"
       />
       <g fill="#4ac683" stroke="rgb(var(--color-secondary)/0.8)" strokeOpacity={0.941}>
         <path
-          markerEnd="url(#g)"
-          markerStart="url(#h)"
-          d="m96.962 100.51-5.826 21.295"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
-        />
-        <path
           markerEnd="url(#i)"
           markerStart="url(#j)"
-          d="m112.36 100.5 2.433 21.293"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="m194.14 100.5 2.373 21.278"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
         <path
           markerEnd="url(#k)"
           markerStart="url(#l)"
-          d="m127.06 100.5 15.51 21.04"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="m208.84 100.5 15.134 21.138"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
         <path
           markerEnd="url(#m)"
           markerStart="url(#n)"
-          d="m139.63 100.5 3.266 20.897"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="m221.41 100.5 2.886 20.978"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
         <path
           markerEnd="url(#o)"
           markerStart="url(#p)"
-          d="m88.087 100.5-14.605 21.03"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="m169.87 100.5-14.726 21.02"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
         <path
           markerEnd="url(#q)"
           markerStart="url(#r)"
-          d="m75.982 100.5-2.856 20.888"
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          d="m157.76 100.5-2.958 20.898"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         />
       </g>
       <g
@@ -386,110 +389,136 @@ const Diagram00 = (props) => (
       >
         <text
           xmlSpace="preserve"
-          x={144.889}
-          y={123.144}
+          x={226.672}
+          y={123.225}
           style={{
             lineHeight: 0,
           }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         >
-          <tspan x={144} y={122}>
+          <tspan x={226} y={122}>
             {'Q'}
           </tspan>
         </text>
         <text
           xmlSpace="preserve"
-          x={140.989}
-          y={125.51}
+          x={222.66}
+          y={125.513}
           style={{
             lineHeight: 0,
           }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         >
-          <tspan x={140.989} y={125.51}>
+          <tspan x={222.66} y={125.513}>
             {'127'}
           </tspan>
         </text>
         <text
           xmlSpace="preserve"
-          x={106.805}
-          y={125.505}
+          x={188.476}
+          y={125.504}
           style={{
             lineHeight: 0,
           }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         >
-          <tspan x={106.805} y={125.505}>
+          <tspan x={188.476} y={125.504}>
             {'0'}
           </tspan>
         </text>
         <text
           xmlSpace="preserve"
-          x={144.939}
+          x={226.721}
           y={101.133}
           style={{
             lineHeight: 0,
           }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         >
-          <tspan x={144.939} y={101.133}>
+          <tspan x={226.721} y={101.133}>
             {'r'}
           </tspan>
         </text>
         <text
           xmlSpace="preserve"
-          x={85.47}
-          y={99.379}
-          style={{
-            lineHeight: 0,
-          }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
-        >
-          <tspan x={85.47} y={99.379}>
-            {'\u03B1 = -1 '}
-          </tspan>
-        </text>
-        <text
-          xmlSpace="preserve"
-          x={124.334}
+          x={167.075}
           y={99.407}
           style={{
             lineHeight: 0,
           }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         >
-          <tspan x={124.334} y={99.407}>
-            {'\xDF = 1'}
+          <tspan x={167.075} y={99.407}>
+            {'\u03B1 = -0.5 '}
           </tspan>
         </text>
         <text
           xmlSpace="preserve"
-          x={106.868}
-          y={99.379}
+          x={205.939}
+          y={99.436}
           style={{
             lineHeight: 0,
           }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         >
-          <tspan x={106.868} y={99.379}>
+          <tspan x={205.939} y={99.436}>
+            {'\xDF = 1.5'}
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          x={177.94}
+          y={99.407}
+          style={{
+            lineHeight: 0,
+          }}
+          transform="translate(-199.077 -127.585) scale(1.3095)"
+        >
+          <tspan x={177.94} y={99.407}>
             {'0'}
           </tspan>
         </text>
         <text
           xmlSpace="preserve"
-          x={70.204}
-          y={125.51}
+          x={188}
+          y={99.407}
           style={{
             lineHeight: 0,
           }}
-          transform="translate(-92.127 -127.577) scale(1.3095)"
+          transform="translate(-199.077 -127.585) scale(1.3095)"
         >
-          <tspan x={70.204} y={125.51}>
-            {'-127'}
+          <tspan x={188} y={99.407}>
+            {'SZ'}
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          x={151.876}
+          y={125.504}
+          style={{
+            lineHeight: 0,
+          }}
+          transform="translate(-199.077 -127.585) scale(1.3095)"
+        >
+          <tspan x={151.876} y={125.504}>
+            {'-128'}
+          </tspan>
+        </text>
+        <text
+          xmlSpace="preserve"
+          x={171.387}
+          y={125.52}
+          style={{
+            lineHeight: 0,
+          }}
+          transform="translate(-199.077 -127.585) scale(1.3095)"
+        >
+          <tspan x={171.387} y={125.52}>
+            {'-Z'}
           </tspan>
         </text>
       </g>
     </g>
   </svg>
 )
-export default Diagram00
+export default Diagram01
