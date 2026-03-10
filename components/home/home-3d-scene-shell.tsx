bdfa5ca8e1bf7dae3ff6ed276ecd65cb09db3f0b
Ankush Patel <patelankush.2012@gmail.com>
2026-03-09 20:16:24 -0700

    bug: disable unstable home 3D runtime path

--- components/home/home-3d-scene-shell.tsx
+++ components/home/home-3d-scene-shell.tsx
@@ -1,19 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-import Home3DErrorBoundary from "@/components/home/home-3d-error-boundary";
 import Home3DFallback from "@/components/home/home-3d-fallback";
 
-const Home3DScene = dynamic(() => import("@/components/home/home-3d-scene"), {
-  ssr: false,
-  loading: () => <Home3DFallback />,
-});
-
 export default function Home3DSceneShell() {
-  return (
-    <Home3DErrorBoundary fallback={<Home3DFallback />}>
-      <Home3DScene />
-    </Home3DErrorBoundary>
-  );
+  return <Home3DFallback />;
 }
